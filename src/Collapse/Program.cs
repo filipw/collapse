0564b2686537727132ef68e94f8b6444cdb60b97
filipw <wojcieszyn@gmail.com>
2022-12-02 07:55:30 +0100

    added sim command and default command

--- src/Collapse/Program.cs
+++ src/Collapse/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Collapse;
 using Spectre.Console.Cli;
 
-var app = new CommandApp<SimulateCommand>();
+var app = new CommandApp();
+app.SetDefaultCommand<SimulateCommand>();
+app.Configure(c =>
+{
+    c.AddCommand<SimulateCommand>("sim");
+});
 return await app.RunAsync(args);

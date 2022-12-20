f22c34b580fde9b6de40fbe130f724d616e71e01
filipw <wojcieszyn@gmail.com>
2022-12-20 19:27:26 +0100

    added new test

--- test/Collapse.Tests/AzureSyncExecutionStrategyTests.cs
+++ test/Collapse.Tests/AzureSyncExecutionStrategyTests.cs
@@ -11,4 +11,20 @@ public class AzureSyncExecutionStrategyTests
         var strategy = new AzureSyncExecutionStrategy(settings);
         Assert.Throws<Exception>(() => strategy.GetExecuteCommandLineInfo("/"));
     }
+
+    [Fact]
+    public void BuildsCorrectCommandFromSettings() 
+    {
+        var settings = new AzureCommandSettings
+        {
+            Path = "/foo",
+            TargetId = "ionq.qpu",
+            Shots = 1024
+        };
+        var strategy = new AzureSyncExecutionStrategy(settings);
+        var result = strategy.GetExecuteCommandLineInfo("/foo");
+
+        Assert.Equal("az", result.Name);
+        Assert.Equal($"quantum execute --project {settings.Path} --target-id {settings.TargetId} --shots {settings.Shots} --output json", result.Args);
+    }
 }

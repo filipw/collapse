1e0d8b9dae474b71bed2e4296cf6f3d593c2ce46
filipw <wojcieszyn@gmail.com>
2022-12-21 16:23:09 +0100

    added first DotnetSimulationStrategyTests

---
+++ test/Collapse.Tests/DotnetSimulationStrategyTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace Collapse.Tests;
+
+public class DotnetSimulationStrategyTests 
+{
+    [Fact]
+    public void ExecuteCommandReturnsExpectedArgumentsForAFolder()
+    {
+        var strategy = new DotnetSimulationStrategy();
+        var result = strategy.GetExecuteCommandLineInfo("/");
+
+        Assert.Equal("run --project / -c Release", result.Args);
+        Assert.Equal("dotnet", result.Name);
+    }
+}

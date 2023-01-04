565aa11f2a9548b52310a1b356cfda18ddf6c5c0
filipw <wojcieszyn@gmail.com>
2023-01-04 20:15:24 +0100

    added QirSimulationStrategy execution test

--- test/Collapse.Tests/QirSimulationStrategyTests.cs
+++ test/Collapse.Tests/QirSimulationStrategyTests.cs
@@ -13,4 +13,18 @@ public class QirSimulationStrategyTests
 
         Assert.Equal(CommandLineInfo.None, result);
     }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("/foo/qir-runner")]
+    public void ExecuteCommandReturnsExpectedArgumentsForAnLlFile(string qirRunnerPath)
+    {
+        var settings = new SimulateCommandSettings { QirRunner = qirRunnerPath};
+        var strategy = new QirSimulationStrategy(settings);
+        //var result = strategy.GetExecuteCommandLineInfo("../../../../../test-assets/H/");
+        var result = strategy.GetExecuteCommandLineInfo("foo.ll");
+
+        Assert.Equal("foo.ll", result.Args);
+        Assert.Equal(string.IsNullOrWhiteSpace(qirRunnerPath) ? "qir-runner" : "/foo/qir-runner", result.Name);
+    }
 }
